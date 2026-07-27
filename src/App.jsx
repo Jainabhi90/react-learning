35b54be7b0685b4f9dd4f6431e79f31010f4a909
Abhi Jain <jainabhi90980@gmail.com>
2026-07-27 13:11:57 +0530

    pta nahi

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,29 @@ import Home from './Home'
 
 const App = () => {
    let [count,setcount] = useState(0)
+   let [colour,setcolour] = useState("red")
+   let [a,seta] = useState(false)
+   let [text,setText] = useState("I'm Spider Man")
+
+   function forToggel(){
+    if(a==false){
+      setcolour("gold")
+      setText("I'm Iron man")
+    }else{
+      setcolour("red")
+      setText("I'm Spider Man")
+    }
+    seta(!a)
+   }
   return (
-    <>
+    <div style={{background:colour,height:"100vh",width:"100vh"}}>
       <h2>{count}</h2>
       <button onClick={()=>{setcount(count+1)}}>add</button>
       <button onClick={()=>{setcount(count-1)}}>sub</button>
       <button onClick={()=>{setcount(0)}}>reset</button>
-      <Home/>
-    </>
+      <button onClick={forToggel}>bgc</button>
+      <h2>{text}</h2>
+    </div>
   )
 }
 
