bb4e7002970a26725721cd049f11df88a1c9a81d
Abhi Jain <jainabhi90980@gmail.com>
2026-07-30 12:40:47 +0530

    class-3

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ import CounterWithLimit from './practice/CounterWithLimit'
 import StudentCard from "./practice/StudentCard";
 import CountSplit1 from './practice/CountSplit1';
 import TodoList from './practice/TodoList';
+import NavBar from './class-3/NavBar';
+import About from './class-3/About';
+import Home from './class-3/Home';
+import Contact from './class-3/Contact';
+;
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
   let[count,setCount] = useState(0);
@@ -11,15 +17,22 @@ const App = () => {
   return (
     <>
     
-    <ForUseEffect/>
+    {/* <ForUseEffect/>
     <CounterWithLimit/>
       <>
        <StudentCard name="Abhi" rollNo={1} city="Indore"/>
        <StudentCard name="Krishiv" rollNo={2} city="Punjab"/>
        <StudentCard name="Chinche" rollNo={3} city="Ujjan"/>
        </>
-    <CountSplit1 a={count} b={setCount} />
-    <TodoList a={list}/>
+    <CountSplit1 a={count} b={setCount} /> */}
+    {/* <TodoList a={list} b={setlist}/> */}
+     <NavBar/>
+      <Routes>
+        <Route path='/' element={<Home/>}> </Route>
+        <Route path='/about' element={<About/>}> </Route>
+        <Route path='/contact' element={<Contact/>}> </Route>
+      </Routes>
+    
     </>
   )
 }
--- src/practice/TodoList.jsx
+++ src/practice/TodoList.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const TodoList = (props) => {
+  let [newTask, setNewTask] = useState("");
+
+  const AddTask = () => {
+    props.b([...props.a, newTask]);
+    setNewTask("");
+  };
+
   return (
     <>
-    <h1>{props.a}</h1>
+      <input
+        type="text"
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+        placeholder="Enter task"
+      />
+      <button onClick={AddTask}>Add</button>
+
+      <ul>
+        {props.a.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
     </>
   )
 }
