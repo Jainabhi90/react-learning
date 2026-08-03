16933c278efbb66359294a94cb1ef67f8f0d96f8
Abhi Jain <jainabhi90980@gmail.com>
2026-08-03 21:59:38 +0530

    class-5.3

--- src/class-5/TodoListByUseReduce.jsx
+++ src/class-5/TodoListByUseReduce.jsx
@@ -2,51 +2,54 @@ import React, { useReducer } from 'react'
 
 const TodoListByUseReduce = () => {
     let data = {
-        input:"",
-        list:[],
-        newInput:"",
+        input: "",
+        list: [],
+        newInput: "",
     }
-    function reduser(state,action){
-        if (action.type=="inp"){
-            return{
+    function reduser(state, action) {
+        if (action.type == "inp") {
+            return {
                 ...state,
-                input:action.payload
+                input: action.payload
             }
-        }else if(action.type=="add"){
-            return{
+        } else if (action.type == "add") {
+            return {
                 ...state,
-                list:[...state.list,state.input],
-                input:""
+                list: [...state.list, state.input],
+                input: ""
             }
-        }else if(action.type=="d"){
-            return{
+        } else if (action.type == "d") {
+            return {
                 ...state,
-                list:state.list.filter((_,id)=>id!==action.payload),
+                list: state.list.filter((_, id) => id !== action.payload),
             }
-        }else if(action.type=="edit"){
-            return{
+        } else if (action.type == "edit") {
+            return {
                 ...state,
-               newInput:state.list.filter((_,id)=>{id==action.payload}),
-               input:{newInput},
+
+                input: state.list[action.payload],
+
+                newInput: state.list[action.payload],
+
             }
         }
     }
-    let[state,dispatch] = useReducer(reduser,data)
-  return (
-    <div>
-        <input value={state.input} onChange={(e)=>{dispatch({type:"inp",payload:e.target.value})}}></input>
-        <button onClick={()=>{dispatch({type:"add"})}}>add</button>
-        {
-            state.list.map((a,i)=>{
-                return <>
-                <li>{a}</li>
-                <button onClick={()=>{dispatch({type:"d",payload:i})}}>del</button>
-                <button onClick={()=>{dispatch({type:"edit",payload:i})}}>edit</button>
-                </>
-            })
-        }
-    </div>
-  )
+    let [state, dispatch] = useReducer(reduser, data)
+    return (
+        <div>
+            <input value={state.input} onChange={(e) => { dispatch({ type: "inp", payload: e.target.value }) }}></input>
+            <button onClick={() => { dispatch({ type: "add" }) }}>add</button>
+            {
+                state.list.map((a, i) => {
+                    return <>
+                        <li>{a}</li>
+                        <button onClick={() => { dispatch({ type: "d", payload: i }) }}>del</button>
+                        <button onClick={() => { dispatch({ type: "edit", payload: i }) }}>edit</button>
+                    </>
+                })
+            }
+        </div>
+    )
 }
 
 export default TodoListByUseReduce
