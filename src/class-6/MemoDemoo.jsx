e723fcd0f7d39dd69b3e32cf39558f1279973eb6
Abhi Jain <jainabhi90980@gmail.com>
2026-08-06 12:53:54 +0530

    local storage

--- src/class-6/MemoDemoo.jsx
+++ src/class-6/MemoDemoo.jsx
@@ -1,8 +1,10 @@
-import React, { useCallback, useMemo, useState } from 'react'
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
 import Child from './child'
 
 const MemoDemoo = () => {
-   let [count,setCount] = useState(0)
+  let [count, setCount] = useState(0)
+  let [inp, setinp] = useState("")
+  let [show, setshow] = useState("noting for now ")
   //  let data = useMemo(()=>{
   //   let res =0;
   //   for(let i=0;i<1000000;i++){
@@ -10,17 +12,44 @@ const MemoDemoo = () => {
   //   }
   //   return res
   //  },[])
-  let demo =()=>{
+  useEffect(() => {
+
+    let store = localStorage.getItem("Key")
+
+    if (store) {
+
+      setinp(JSON.parse(store))
+
+      setshow(JSON.parse(store))
+
+    }
+
+  }, [])
+  let demo = () => {
     console.log("hellooooo");
-    
+
   }
-  let fun = useCallback(demo,[])
+  let fun = useCallback(demo, [])
   return (
     <div>
-          {/* <h2> data :{data}</h2> */}
-         <h1>{count}</h1>
-        <button onClick={()=>(setCount(count+1))} >click</button>
-        <Child fun={fun}/>
+      {/* <h2> data :{data}</h2> */}
+      <input
+
+        value={inp}
+
+        onChange={(e) => setinp(e.target.value)}
+
+      />
+      <h1>{show}</h1>
+      <button
+
+        onClick={() => {
+          setshow(inp)
+          localStorage.setItem("Key", JSON.stringify(inp))
+        }}
+
+      >click</button>
+      <Child fun={fun} />
     </div>
   )
 }
